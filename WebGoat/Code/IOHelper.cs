a5f0c68f6971b1cffc9dd2e52e99e7f2b00648ad
Feliciano Kava <feliciano.kava@ambevtech.com.br>
2022-08-31 15:53:13 -0300

    first commit

---
+++ WebGoat/Code/IOHelper.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace OWASP.WebGoat.NET
+{
+    // VAI!
+    public class IOHelper
+    {
+        public static string ReadAllFromFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string data = sr.ReadToEnd();
+            sr.Close();
+            return data;
+        }
+    }
+}
